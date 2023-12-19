9fd60ad9964edfa3f85c1b5169c6ef6db5723ebf
Zach Frankel <zachif1997@gmail.com>
2023-12-19 14:40:05 -0600

    user login and registration

---
+++ flask_app/controllers/users_controller.py
@@ -0,0 +1,37 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models.users_model import Users
+
+@app.route('/WeCommerce')
+def home():
+    return render_template('index.html')
+
+@app.route("/register", methods=["POST"])
+def register():
+    data ={
+        "first_name" : request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"],
+        "password" : request.form["password"],
+        "confirm_password" : request.form["confirm_password"]
+    }
+    valid=Users.register_user(data)
+    print('_________________________________________________________')
+    print (valid)
+    if not valid:
+        return redirect('/WeCommerce')
+    current_user=Users.login_user(data)
+    session['id']=current_user
+    return redirect('/WeCommerce/dashboard')
+
+@app.route('/login', methods=['POST'])
+def login():
+    data={
+        "email" : request.form["lemail"],
+        "password" : request.form["lpassword"]
+    }
+    current_user=Users.login_user(data)
+    if not current_user:
+        return redirect('/WeCommerce')
+    session['id']=current_user
+    return redirect('/WeCommerce/dashboard')
