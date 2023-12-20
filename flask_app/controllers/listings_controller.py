28ef8cd5705ca2ad347614e5a3ac3a915de554e8
Breelin Clark <breelinclark09@gmail.com>
2023-12-20 15:01:42 -0600

    Bupdate4

--- flask_app/controllers/listings_controller.py
+++ flask_app/controllers/listings_controller.py
@@ -7,30 +7,29 @@ from flask_app.models.listings_model import Listings
 def create_listings():
     return render_template('create_listing.html')
 
-@app.route('/...', methods=['POST'])
+@app.route('/listing', methods=['POST'])
 def create_listing_form():
     if not Listings.createListing(request.form):
         return redirect('/create_listings')
     data = { "name": request.form['name'],
             "description": request.form['descripton'],
             "price": request.form['price'],
-            "seller_id": request.form['seller_id']
+            "seller": session['id']
             }
     Listings.insertListing(data)
     return redirect('/create_listings')
 
 @app.route('/edit')
-def edit_listing(listing_id):
+def edit_listing(id):
     data = {
-        'seller_id' : listing_id
+        'id' : id
     }
-    session['listing_id'] = listing_id
     return render_template('edit.html', listing = Listings.findListingByID(data))
 
-@app.route('/delet_listing')
-def deleteListing(listing_id):
+@app.route('/delete_listing')
+def deleteListing(id):
     data = {
-            "seller_id": listing_id
+            "id": id
             }
     Listings.deleteListing(data)
     return redirect('/create_listing')
