feb003860bc9ca3e48c7260892bf0bfeff9aec02
Zach Frankel <zachif1997@gmail.com>
2023-12-20 21:58:42 -0600

    message stuff

--- flask_app/controllers/messages_controller.py
+++ flask_app/controllers/messages_controller.py
@@ -2,4 +2,15 @@ from flask_app import app
 from flask import render_template, redirect , request , session, flash
 from flask_app.models.users_model import Users
 from flask_app.models.listings_model import Listings
+from flask_app.models.messages_model import Messages
 
+@app.route('/WeCommerce/Messages/<int:correspondent>')
+def message(correspondent):
+    session['correspondent'] = correspondent
+    data={
+        'user' : session['id'],
+        'corrispondant' : session['correspondent']
+    }
+    messages=Messages.retrieveMessagesByUsers(data)
+    return render_template("message_user.html")
+    
